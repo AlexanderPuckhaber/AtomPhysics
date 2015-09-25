043427a7b263c51fa48062950d10de3366d9222b
Alexander Puckhaber <alexpuckhaber@gmail.com>
2015-09-25 10:54:42 -0400

    actually adding the buttons because their classes didn't commit

---
+++ src/AlexanderP/Button.java
@@ -0,0 +1,84 @@
+package AlexanderP;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.geom.Rectangle2D;
+
+public class Button 
+{
+	double x, y, w, h;
+	Rectangle2D.Double r;
+	boolean pressed, visible;
+	String s;
+	Color c, oldC;
+	
+	public Button(double nX, double nY, double nW, double nH, String nS, Color nC)
+	{
+		x = nX;
+		y = nY;
+		w = nW;
+		h = nH;
+		
+		r = new Rectangle2D.Double(x, y, w, h);
+		s = nS;
+		c = nC;
+		oldC = c;
+		
+		pressed = false;
+	}
+	
+	public void checkPressed(Point p)
+	{
+		if (r.contains(p))
+		{
+			pressed = true;
+		}
+	}
+	
+	public void checkHover(Point p)
+	{
+		if (r.contains(p))
+		{
+			
+			int tmpR = c.getRed();
+			int tmpG = c.getGreen();
+			int tmpB = c.getBlue();
+			
+			
+			tmpR += 20;
+			if (tmpR > 255)
+				tmpR = 255;
+			
+			tmpG += 20;
+			if (tmpG > 255)
+				tmpG = 255;
+			
+			c = new Color(tmpR, tmpG, tmpB);
+		}
+		else
+		{
+			c = oldC;
+		}
+	}
+	
+	public String getDescription()
+	{
+		return s;
+	}
+	
+	public boolean getPressed()
+	{
+		return pressed;
+	}
+	
+	public void draw(Graphics2D g)
+	{
+		g.setColor(c);
+		g.fill3DRect((int)x, (int)y, (int)w, (int)h, true);
+		g.setColor(Color.BLACK);
+		g.drawString(s, (int)x, (int)y+10+(int)(h/2));
+	}
+	
+
+}
---
+++ src/AlexanderP/ButtonManager.java
@@ -0,0 +1,77 @@
+package AlexanderP;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.util.ArrayList;
+
+public class ButtonManager 
+{
+
+	
+	public ButtonManager()
+	{
+		
+		
+	}
+	
+	public ArrayList<Button> createButtons()
+	{
+		
+		ArrayList<Button> buttonList = new ArrayList<Button>();
+		
+		Button goopButton = new Button(10, 450, 100, 50, "Goop ($10)", new Color(100, 100, 100));
+		Button concreteButton = new Button(110, 450, 100, 50, "Concrete ($20)", new Color(100, 100, 100));
+		Button steelButton = new Button(210, 450, 100, 50, "Steel ($50)", new Color(100, 100, 100));
+		Button liquidButton = new Button(310, 450, 100, 50, "Liquid ($2)", new Color(100, 100, 100));
+		
+		buttonList.add(goopButton);
+		buttonList.add(concreteButton);
+		buttonList.add(steelButton);
+		buttonList.add(liquidButton);
+		
+		return buttonList;
+	}
+	
+	public void draw(Graphics2D g, ArrayList<Button> buttonList)
+	{
+		for (int i = 0; i < buttonList.size(); i++)
+		{
+			buttonList.get(i).draw(g);
+		}
+	}
+	
+	public void checkButtons(ArrayList<Button> buttonList, Point p)
+	{
+		for (int i = 0; i < buttonList.size(); i++)
+		{
+			buttonList.get(i).checkPressed(p);
+		}
+	}
+	
+	public void checkButtonHover(ArrayList<Button> buttonList, Point p)
+	{
+		for (int i = 0; i < buttonList.size(); i++)
+		{
+			buttonList.get(i).checkHover(p);
+		}
+	}
+	
+	public boolean findButtonPressed(ArrayList<Button> buttonList, String desc)
+	{
+		int i = 0;
+		boolean found = false;
+		while (i < buttonList.size() && !found)
+		{
+			if (buttonList.get(i).getDescription().equalsIgnoreCase(desc))
+			{
+				found = true;
+			}
+				
+			i++;
+		}
+		
+		return buttonList.get(i-1).getPressed();
+	}
+	
+}
